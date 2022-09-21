f387fc22edc139c8f14a5dd2b27475a7baa669e0
Dozy <carlos@devhive.mx>
2022-09-21 00:45:52 -0500

    Se agrego formulario de login en loginComponent & Estilos, ademas se codifico logica de FormBuilder

--- src/app/pages/components/auth/login/login.component.ts
+++ src/app/pages/components/auth/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +8,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  public formlogin: FormGroup;
+  constructor(private formBuilder: FormBuilder,private router:Router) {
+    ///
 
-  constructor() { }
+    this.formlogin = this.formBuilder.group({
+      email: ['', [
+        Validators.required,
+        Validators.maxLength(30),
+        Validators.minLength(3),
+        Validators.email
+      ]],
+      password: ['', [
+        Validators.required,
+        Validators.minLength(8)
+      ]]
+    })
+
+    //
+   }
 
   ngOnInit(): void {
+  }
+
+  submit = () => {
+    this.router.navigate(['/pages']);
   }
 
 }
--- src/app/pages/components/auth/login/login.module.ts
+++ src/app/pages/components/auth/login/login.module.ts
@@ -3,13 +3,21 @@ import { CommonModule } from '@angular/common';
 import { LoginComponent } from './login.component';
 import { loginRoutingModule } from './login-routing.module';
 
-
-
+import { MatCardModule } from '@angular/material/card'
+import { MatButtonModule } from '@angular/material/button'
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 @NgModule({
   declarations: [LoginComponent],
   imports: [
     CommonModule,
-    loginRoutingModule
+    loginRoutingModule,
+    ReactiveFormsModule,
+    MatCardModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule
   ]
 })
 export class LoginModule { }
