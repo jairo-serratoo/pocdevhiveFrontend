61132fbe5441939b4309a1009645ac4056af56df
Dozy <carlos@devhive.mx>
2022-09-20 23:53:26 -0500

    Configuracion de rutas en app-routing.module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginModule } from './pages/components/auth/login/login.module';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'pages',
+    loadChildren: () => import('./pages/shared/menu/menu.module').then(m => m.MenuModule),
+  },
+  {
+    path: 'login',
+    loadChildren: () => import('./pages/components/auth/login/login.module').then(m => m.LoginModule),
+  },
+  {
+    path: '',
+    pathMatch: 'full', 
+    redirectTo: 'login'
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
