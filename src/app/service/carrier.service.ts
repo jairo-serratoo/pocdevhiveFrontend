02d08727bbf80f2bcce0882093a75246c2f7b1ed
Dozy <carlos@devhive.mx>
2022-09-26 09:33:12 -0500

    prueba en carrierService para persistir datos (funciona pero se va a modificar)

--- src/app/service/carrier.service.ts
+++ src/app/service/carrier.service.ts
@@ -1,17 +1,27 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { Iuser } from '../interfaces/iuser';
 import { Carrier } from '../models/carrier';
+import { User } from '../models/user';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CarrierService {
 
+  user!:Iuser
+
   baseUrl:string = 'https://springboot-poc-devhive.azurewebsites.net'
   cabecera: HttpHeaders = new HttpHeaders({ 'Content-Type': 'application/json' });
   constructor(private http:HttpClient) { }
 
+  getUser = () => {
+    return this.user
+  }
+  setUser = (user:User) => {
+    return this.user = user
+  }
   getCarriers$():Observable<Carrier[]>{
     return this.http.get<Carrier[]>(`${this.baseUrl}/tracking`)
   }
