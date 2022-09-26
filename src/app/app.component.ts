97bbe3c3e653e939d38f1a3d7818d34cb9e084de
Dozy <carlos@devhive.mx>
2022-09-26 09:36:35 -0500

    se implemento carrierService en appComponent para saber si fuciona en produccion (Azure)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Carrier } from './models/carrier';
+import { CarrierService } from './service/carrier.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'Angular 13';
+  list:Carrier[]
+  constructor(private carrierService:CarrierService){
+    this.list = []
+  }
+  
+  ngOnInit(): void {
+    this.carrierService.getCarriers$().subscribe({
+      next:(c:Carrier[])  => {
+        console.info('Carrier',c)
+        this.list = c
+      },
+      error:(err)  => {
+        console.log(err)
+      },
+      complete:()  => {
+        console.log('complete()')
+      },
+    })
+  }
 }
